8917d7b72f146be5e89a0f720a677a4b78e80de0
sunayana-lakki <sunayana.lakki@techdenali.com>
2026-07-28 16:32:08 +0530

    add resuable css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import '@rentbook/rentbook-ui-lib/microfrontend.min.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+function App() {
+  const queryClient = new QueryClient()
 
   return (
     <>
-
+      <QueryClientProvider client={queryClient}>
+        Agent Widget
+      </QueryClientProvider>
     </>
   )
 }
